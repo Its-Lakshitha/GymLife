6a8347d052d1311822f4a65e64633283e7903a26
Lakshitha Fernando <avishkalakshitha0502@gmail.com>
2026-05-17 09:01:21 +0530

    Update role model and the migration

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    use HasFactory, HasUlids;
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+    ];
+
+
+    public function users(){
+        return $this->hasMany(User::class)
+    }
 }
